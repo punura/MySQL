c87d0f23b3fdba29199ee89665bd752781a25d59
Punura Wathsara <123pemmawadupunura@gmail.com>
2023-03-09 11:44:31 +0530

    Added New File

---
+++ Creating a Copy of Table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE invoices_archive AS
+
+SELECT 
+	i.invoice_id,
+    i.number,
+    c.name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.payment_date,
+    i.due_date
+FROM invoices i
+JOIN clients c
+	USING (client_id)
+WHERE i.payment_date IS NOT NULL
+
+
+	
