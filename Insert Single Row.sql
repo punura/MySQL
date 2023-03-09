f6d245ef5af9494b526237488b077b5fdb305bf0
Punura Wathsara <123pemmawadupunura@gmail.com>
2023-03-09 11:12:08 +0530

    Added New Files

---
+++ Insert Single Row.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers (
+    last_name,
+	first_name,
+    birth_date,
+    address,
+    city,
+    state)
+VALUES (
+    'Smith',
+    'Jhon',
+    '1990-01-01',
+    'address',
+    'city',
+    'CA')
